ab0e37c0c503e7c95fbb62c680eb6eaa6f288e55
Calvin Jacobsen <Jaco1932@umn.edu>
2021-05-08 15:51:33 -0500

    setting up new project repo

---
+++ models/Workouts.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+class Comment extends Model { }
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_date: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        comment_content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
